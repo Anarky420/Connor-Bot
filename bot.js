6ab11dc606bb41d7127bc9466f31c2ccb2f19ad8
Anarky420 <39548876+Anarky420@users.noreply.github.com>
2018-08-17 20:04:24 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,11 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'Hello') {
+    	message.channel.send('Hello!');
   	}
 });
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
